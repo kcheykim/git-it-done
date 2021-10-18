be67e1c655e3d3e5d0b5d30f22f63a4a1c1ad18c
kcheykim <kosalcheykim@gmail.com>
2021-10-18 13:15:42 -0700

    chnages to fetch user's repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,28 @@
-var getUserRepos = function() {
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+
    // console.log("function was called");
-   fetch("https://api.github.com/users/octocat/repos");
-  };
+  /* var response = fetch("https://api.github.com/users/octocat/repos");
+   console.log(response);
+  };*/
+  /*
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  //console.log("inside", response);
+  response.json().then(function(data) {
+    console.log(data);
+ // } */
+});
+});
+}
+//console.log("outside");
   
-  getUserRepos();
+  //getUserRepos();
+  getUserRepos("microsoft");
+  getUserRepos("facebook");
+  getUserRepos("kcheykim");
